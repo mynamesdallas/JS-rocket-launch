b977b1f5d02c4983e4fff67979ebc6b37c63a2c8
Dallas <dallas.mockridge@yahoo.com>
2020-08-10 18:54:14 -0400

    Made my rocket

--- rocket.js
+++ rocket.js
@@ -2,22 +2,34 @@ var timer = null;
 var countdownNumber = 10;
 
 var changeState = function (state) {
-    document.body.className = 'body-state'+ state;
+    document.body.className = 'body-state'+state;
     clearInterval(timer);
+    countdownNumber = 10;
+    document.getElementById('countdown').innerHTML = countdownNumber;
 
     if (state == 2) {
         timer = setInterval(function () {
-            document.getElementById('countdown').innerHTML = countdownNumber;
             countdownNumber = countdownNumber -1;
-            if (countdownNumber <= 0) {
+            document.getElementById('countdown').innerHTML = countdownNumber;
+           
+            if (countdownNumber <=0) {
+                
                 changeState(3);
             };
-        }, 1000);
-       
+
+        },1000);
+               
     } else if (state == 3) {
-        var success = setTimeout (funtion (),
-        {
-        
+        var success = setTimeout(function () {
+            var randomNumber = Math.round(Math.random()*10);
+            console.log('randomNumber:' , randomNumber)
+
+            if (randomNumber >5 ) {
+                changeState(4);
+            } else {
+                changeState(5);
+            }
+
         }, 2000);
     };
 }
